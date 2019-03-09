a26ee5d39b579789249589c82d73d24ed4bc62c9
Danila Nikonets <danila.nikonets@gmail.com>
2019-03-09 13:39:38 +0500

    In-app purchases promotion

--- InAppUtils/InAppUtils.m
+++ InAppUtils/InAppUtils.m
@@ -6,8 +6,14 @@
 
 @implementation InAppUtils
 {
+    bool hasListeners;
+    
     NSArray *products;
     NSMutableDictionary *_callbacks;
+    
+    //Promoted in-app purchase
+    SKProduct *promotedProduct;
+    SKPayment *promotedProductPayment;
 }
 
 - (instancetype)init
@@ -29,6 +35,19 @@
 }
 
 RCT_EXPORT_MODULE()
+
+- (NSArray<NSString *> *)supportedEvents
+{
+    return @[@"OnPromotedProduct"];
+}
+
+-(void)startObserving {
+    hasListeners = YES;
+}
+
+-(void)stopObserving {
+    hasListeners = NO;
+}
 
 - (void)paymentQueue:(SKPaymentQueue *)queue
  updatedTransactions:(NSArray *)transactions
@@ -128,7 +147,7 @@ restoreCompletedTransactionsFailedWithError:(NSError *)error
                 callback(@[@"restore_failed"]);
                 break;
         }
-
+        
         [_callbacks removeObjectForKey:key];
     } else {
         RCTLogWarn(@"No callback registered for restore product request.");
@@ -256,8 +275,35 @@ RCT_EXPORT_METHOD(receiptData:(RCTResponseSenderBlock)callback)
         purchase[@"originalTransactionDate"] = @(originalTransaction.transactionDate.timeIntervalSince1970 * 1000);
         purchase[@"originalTransactionIdentifier"] = originalTransaction.transactionIdentifier;
     }
-
+    
     return purchase;
+}
+
+#pragma mark In-app purchases promotion
+
+- (BOOL)paymentQueue:(SKPaymentQueue *)queue shouldAddStorePayment:(SKPayment *)payment forProduct:(SKProduct *)product {
+    promotedProduct = product;
+    promotedProductPayment = payment;
+    if(hasListeners) {
+        [self sendEventWithName:@"OnPromotedProduct" body:payment.productIdentifier];
+    }
+    return false;
+}
+
+RCT_EXPORT_METHOD(getPromotedProduct: (RCTResponseSenderBlock)callback)
+{
+    callback(@[promotedProduct ? promotedProduct.productIdentifier : [NSNull null]]);
+}
+
+RCT_EXPORT_METHOD(buyPromotedProduct:(RCTResponseSenderBlock)callback)
+{
+    if(promotedProductPayment) {
+        [[SKPaymentQueue defaultQueue] addPayment:promotedProductPayment];
+        _callbacks[RCTKeyForInstance(promotedProductPayment.productIdentifier)] = callback;
+    }
+    else {
+        callback(@[@"no_initial_product"]);
+    }
 }
 
 - (void)dealloc
